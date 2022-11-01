59e5e7e7a5368510a3d7a99e37def95610842ef5
jgrissom <jgrissom91@gmail.com>
2022-11-01 11:19:35 -0500

    discounts

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Northwind.Models;
 using System.Linq;
+using System;
 
 namespace Northwind.Controllers
 {
@@ -11,5 +12,6 @@ namespace Northwind.Controllers
         public ProductController(NorthwindContext db) => _northwindContext = db;
         public IActionResult Category() => View(_northwindContext.Categories.OrderBy(c => c.CategoryName));
         public IActionResult Index(int id) => View(_northwindContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
+        public IActionResult Discounts() => View(_northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now));
     }
 }
